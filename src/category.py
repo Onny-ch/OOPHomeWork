6514e124ed5aef0f015a83d0e9fc9950b5aca0cf
Andrey Osipov <bezy4uy@gmail.com>
2024-10-09 18:22:14 +0300

    add some functionality and do private products attribute

--- src/category.py
+++ src/category.py
@@ -8,9 +8,22 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name: str, description: str, products: list[Product]) -> None:
+    def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, new_product: Product):
+        """Метод добавления нового продукта"""
+        self.__products.append(new_product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Геттер получения продуктов построчно"""
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} Остаток: {product.quantity}\n"
+        return products_str

a523f862fcf5d79f9a2235ab836f0c5b29e036a4
Andrey Osipov <bezy4uy@gmail.com>
2024-10-10 13:17:58 +0300

    added tests for product class

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,8 +15,38 @@ def product1():
 
 
 @pytest.fixture
+def product1_str():
+    return "Samsung Galaxy S23 Ultra, 180000.0 Остаток: 5\n"
+
+
+@pytest.fixture
+def product1_dict():
+    return {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 200000.0,
+        "quantity": 5
+    }
+
+
+@pytest.fixture
 def product2():
     return Product(name="Iphone 15", description="512GB, Gray space", price=210000.0, quantity=8)
+
+
+@pytest.fixture
+def product2_dict():
+    return {
+        "name": "Iphone 17",
+        "description": "1024Tb, Gray space",
+        "price": 1600000.0,
+        "quantity": 1
+    }
+
+
+@pytest.fixture
+def product2_str():
+    return "Iphone 15, 210000.0 Остаток: 8\n"
 
 
 @pytest.fixture
@@ -26,6 +56,21 @@ def product3():
         description="1024GB, Синий",
         price=31000.0,
         quantity=14,
+    )
+
+
+@pytest.fixture
+def product3_str():
+    return "Xiaomi Redmi Note 11, 31000.0 Остаток: 14\n"
+
+
+@pytest.fixture
+def product4():
+    return Product(
+        name="55\" QLED 4K",
+        description="Фоновая подсветка",
+        price=123000.0,
+        quantity=7
     )
 
 
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,7 @@
+from src.product import Product
+from unittest.mock import patch
+
+
 def test_product_init(product1, product2, product3):
     assert product1.name == "Samsung Galaxy S23 Ultra"
     assert product1.description == "256GB, Серый цвет, 200MP камера"
@@ -13,3 +17,31 @@ def test_product_init(product1, product2, product3):
     assert product3.description == "1024GB, Синий"
     assert product3.price == 31000.0
     assert product3.quantity == 14
+
+
+def test_new_product(product1, product2, product1_dict, product2_dict):
+    new_product1 = Product.new_product(product1_dict)
+    assert new_product1.quantity == 10
+    assert new_product1.price == 200000.0
+    new_product2 = Product.new_product(product2_dict)
+    assert new_product2.name == "Iphone 17"
+    assert new_product2.description == "1024Tb, Gray space"
+    assert new_product2.price == 1600000.0
+    assert new_product2.quantity == 1
+
+
+def test_product_private_price(capsys, product1):
+    product1.price = 0
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
+
+    product1.price = 190000
+    assert product1.price == 190000
+
+    with patch('builtins.input', side_effect=['n']):
+        product1.price = 160000
+        assert product1.price == 190000
+
+    with patch('builtins.input', side_effect=['y']):
+        product1.price = 160000
+        assert product1.price == 160000

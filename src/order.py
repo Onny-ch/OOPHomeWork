78d9b6d53b54b6c2f962eb7b06aa254cbfa4392d
Andrey Osipov <bezy4uy@gmail.com>
2024-10-21 19:01:57 +0300

    final order changes

--- src/order.py
+++ src/order.py
@@ -6,8 +6,7 @@ class Order(BaseOrder):
     description: str
     price: float
     quantity: int
-    # max_limit_order: bool = True
-    # current_order_count: bool = False
+    current_order = None
 
     def __init__(self, product: str, description: str, price: float, quantity: int) -> None:
         self.product = product
@@ -15,7 +14,16 @@ class Order(BaseOrder):
         self.price = price
         self.quantity = quantity
         super().__init__()
-        # self.current_order_count = True
+
+    def __new__(cls, *args, **kwargs):
+        if cls.current_order is None:
+            cls.current_order = super().__new__(cls)
+        return cls.current_order
 
     def __str__(self) -> str:
-        return f"Товар: {self.product}, количество: {self.quantity}, цена: {self.price}"
+        return (
+            f"Товар: {self.product}, "
+            f"информация о товаре: {self.description}, "
+            f"количество: {self.quantity}, "
+            f"цена: {self.price}"
+        )

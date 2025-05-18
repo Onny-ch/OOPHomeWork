d740c679d5c7ba235e5ac42ccbf76df4d9a01622
Andrey Osipov <bezy4uy@gmail.com>
2025-05-18 13:10:34 +0300

    added calculating the average price of all goods

--- src/category.py
+++ src/category.py
@@ -39,3 +39,10 @@ class Category(BaseOrder):
         for product in self.__products:
             products_str += str(product) + "\n"
         return products_str
+
+    def average_price_of_goods(self):
+        """Подсчет среднего ценника всех товаров в классе Category"""
+        try:
+            return round(sum([product.price for product in self.__products]) / len(self.__products), 1)
+        except ZeroDivisionError:
+            return 0
